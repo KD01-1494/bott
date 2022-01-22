1761e3c590c24b14ad958b3d99abce641e7f8d27
Alexey <91065165+KD01-1494@users.noreply.github.com>
2022-01-22 15:27:43 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,65 +3,93 @@
 define('TOKEN', '2081875841:AAHdbrbgvzpw45UUurICDETnXywMSNkVR84');
 define('ADMIN_ID', '1841417011');
 
-$data = file_get_contents('php://input');
-$data = json_decode($data, true);
-
+$data = json_decode(file_get_contents('php://input'), TRUE);
 
 if (!isset($data)) {
-
-	try {
-		$str = fopen("form.txt", 'r');
-		echo $str;
-	} catch (Exception $e) {
-		die('Не открыть( нет в директории');	
-	}
-
-	exit();
+	die(':0');
 }
 
-// Вызов к API Методам.
-function sendTelegram($method, $response)
-{
-	$ch = curl_init('https://api.telegram.org/bot' . TOKEN . '/' . $method);  
-	curl_setopt($ch, CURLOPT_POST, true);  
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	$ex = curl_exec($ch);
-
-	$fd = fopen("form.txt", 'a') or die("не удалось открыть файл");
-	fwrite($fd, $ex);
-	fclose($fd);
+$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
 
 
-	curl_close($ch);
-}
-
-// Основные Команды
-switch ($data['message']['text']) {
-	case '/start':
-		sendTelegram(
-			'sendMessage', 
-			[
-				'chat_id' => $data['message']['chat']['id'],
-				'text' => 'Что вы хотите заказать?',
-				'replyKeyboardMarkup' => [
-					'rows' => [
-						['text' => 'jello1'],
+switch ($message) {
+    case 'да':
+        $method = 'sendMessage';
+		$send_data = [
+			'text' => 'Что вы хотите заказать?',
+			'reply_markup'  => [
+				'resize_keyboard' => true,
+				'keyboard' => [
+						[
+							['text' => 'Яблоки'],
+							['text' => 'Груши'],
+						],
+						[
+							['text' => 'Лук'],
+							['text' => 'Чеснок'],
+						]
 					]
 				]
-			]
-		);
-
-	// Админ панель
-	case '/admin':
-		if ($data['message']['chat']['id'] != ADMIN_ID) {
-		 	sendTelegram(
-				'sendMessage', 
-				[
-					'chat_id' => $data['message']['chat']['id'],
-					'text' => 'Вы не администратор!'
+			];
+    break;
+	case 'нет':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'Приходите еще'];
+    break;
+	case 'яблоки':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'груши':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'лук':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'чеснок':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	default:
+		$method = 'sendMessage';
+		$send_data = [
+			'text' => 'Вы хотите сделать заказ?',
+			'reply_markup'  => [
+				'resize_keyboard' => true,
+				'keyboard' => [
+						[
+							['text' => 'Да'],
+							['text' => 'Нет'],
+						]
+					]
 				]
-			);
-		 }
+			];
 }
+
+$send_data['chat_id'] = $data['chat'] ['id'];
+
+$res = sendTelegram($method, $send_data);
+
+
+
+
+function sendTelegram($method, $data, $headers = [])
+{
+	$curl = curl_init();
+	curl_setopt_array($curl, [
+		CURLOPT_POST => 1,
+		CURLOPT_HEADER => 0,
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+		CURLOPT_POSTFIELDS => json_encode($data),
+		CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"))
+	]);
+	$result = curl_exec($curl);
+	curl_close($curl);
+	return (json_decode($result, 1) ? json_decode($result, 1) : $result);
+}
+
+?>
